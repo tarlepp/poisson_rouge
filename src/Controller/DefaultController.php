5111dff873bff6331979893eafc464127b5cc6cd
tarlepp <tarmo.leppanen@protacon.com>
2018-12-01 19:30:57 +0200

    how to use DI

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,37 @@
 
 namespace App\Controller;
 
+use App\Service\BarService;
+use App\Service\FooService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="default")
+     * @var \App\Service\FooService
      */
-    public function index()
+    private $fooService;
+
+    public function __construct(FooService $fooService)
+    {
+        $this->fooService = $fooService;
+    }
+
+    /**
+     * @Route("/", name="default")
+     *
+     * @param BarService $barService
+     *
+     * @return Response
+     */
+    public function index(BarService $barService): Response
     {
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            'foo' => $this->fooService->foo(),
+            'bar' => $barService->bar(),
         ]);
     }
 }
---
+++ src/Service/FooService.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service;
+
+class FooService
+{
+    public function foo(): string
+    {
+        return 'foo';
+    }
+}
